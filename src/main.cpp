eaeb8757e68fbee24bbe46af88ed6a420507a05f
NoamPrag <noam.prag07@gmail.com>
2020-11-09 03:33:04 +0200

    Included classes in main file and tested with println to serial

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+#include <./Turret/Turret.h>
+#include <./Shooter/Shooter.h>
+#include <LED/LED.h>
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Working! :)");
+}
+
+void loop()
+{
+}
