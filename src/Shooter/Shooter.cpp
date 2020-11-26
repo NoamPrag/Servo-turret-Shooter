9fb29b082aa50295a98a3787b1a1b0b39834e7d7
NoamPrag <noam.prag07@gmail.com>
2020-11-26 13:12:51 +0200

    Added shoot function for Shooter

--- src/Shooter/Shooter.cpp
+++ src/Shooter/Shooter.cpp
@@ -4,3 +4,12 @@ Shooter::Shooter(const int pin)
 {
     this->attach(pin);
 };
+
+void Shooter::shoot()
+{
+    this->write(shootAngle); // Pull the trigger
+
+    delay(500); // Wait half a second
+
+    this->write(releaseAngle); // Turn back to the default angle
+};
--- src/Shooter/Shooter.h
+++ src/Shooter/Shooter.h
@@ -1,7 +1,14 @@
 #include <Servo.h>
+#include <Arduino.h>
 
 class Shooter : Servo
 {
+private:
+    static const float releaseAngle;
+    static const float shootAngle;
+
 public:
     Shooter(const int pin);
+
+    void shoot();
 };
