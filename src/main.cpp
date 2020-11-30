eaa18c9593f8bdda4dde292ee87c3a14b6364961
NoamPrag <noam.prag07@gmail.com>
2020-11-30 11:56:27 +0200

    Made all variables and constants static. Ended serial communication at setup function. Returning from loop function if shooting, because everything else is unnecessary in this case. startTargetAngle is equal to current angle, in order to not call turret.getAngle() twice.

--- src/main.cpp
+++ src/main.cpp
@@ -5,73 +5,51 @@
 #include <./Shooter/Shooter.h>
 #include <LED/LED.h>
 
-const int turretPin = 0;
-const int shooterPin = 0;
-const int ultrasonicTrigPin = 0;
-const int ultrasonicEchoPin = 0;
+static const int turretPin = 0;
+static const int shooterPin = 0;
+static const int ultrasonicTrigPin = 0;
+static const int ultrasonicEchoPin = 0;
 
-const int redLEDPin = 0;
-const int greenLEDPin = 0;
-const int blueLEDPin = 0;
+static const int redLEDPin = 0;
+static const int greenLEDPin = 0;
+static const int blueLEDPin = 0;
 
-const int buttonPin = 0; // TODO: Add button to confirm shooting.
+static const int buttonPin = 0; // TODO: Add button to confirm shooting.
 
-const float turretDAngle = 0; // TODO: Add real value.
+static const float turretDAngle = 0; // TODO: Add real value.
 
-LED led(9);
+static LED led(9);
 
-Turret turret(turretPin, ultrasonicTrigPin, ultrasonicEchoPin);
-Shooter shooter(shooterPin);
+static Turret turret(turretPin, ultrasonicTrigPin, ultrasonicEchoPin);
+static Shooter shooter(shooterPin);
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Working! :)");
+  Serial.print("Working! :)");
+  Serial.end();
 
   turret.reset(); // Turn the turret to the min angle.
+  shooter.reset();
 };
 
-bool prevIsOnTarget = false;
+static bool prevIsOnTarget = false;
 
-float startTargetAngle = 0;
-float startTargetDistance = 0;
-float endTargetDistance = 0;
-float endTargetAngle = 0;
+static float startTargetAngle = 0;
+static float startTargetDistance = 0;
+static float endTargetDistance = 0;
+static float endTargetAngle = 0;
 
-bool isShooting = false;
+static bool isShooting = false;
 
-float centerOfTarget = 0;
+static float centerOfTarget = 0;
 
 // TODO: Add LED logic.
 
 void loop()
 {
-  // Measure distance.
-  const float measuredDistance = turret.readDistance();
-
-  // 2) If dropping edge of being on target -> go to center and shoot. else -> keep searching.
-  const float isOnTarget = turret.isOnTarget();
-
-  const float currentAngle = turret.getAngle();
-
-  if (isOnTarget)
-  {
-    if (!prevIsOnTarget) // Rising edge
-      startTargetAngle = turret.getAngle();
-
-    endTargetDistance = measuredDistance; // gets updated all the time until looses target.
-    endTargetAngle = currentAngle;
-  }
-
-  else if (prevIsOnTarget) // Dropping edge
-  {
-    isShooting = true;
-    centerOfTarget = turret.getCenterOfTarget(startTargetAngle, startTargetDistance, endTargetAngle, endTargetDistance);
-  }
-
   if (isShooting)
   {
-
     turret.setAngle(centerOfTarget);
 
     if (turret.isReadyToShoot(centerOfTarget))
@@ -79,8 +57,35 @@ void loop()
       shooter.shoot();
       isShooting = false;
     }
+
+    return;
   }
-  else
-    // Turn turret dAngle degrees.
-    turret.turn(turretDAngle);
+
+  // Turn turret dAngle degrees.
+  turret.turn(turretDAngle);
+
+  // Measure distance.
+  const float measuredDistance = turret.readDistance();
+
+  // If dropping edge of being on target -> go to center and shoot; else -> keep searching.
+  const float isOnTarget = turret.isOnTarget();
+
+  const float currentAngle = turret.getAngle();
+
+  if (isOnTarget)
+  {
+    if (!prevIsOnTarget) // Rising edge
+    {
+      startTargetAngle = currentAngle;
+    }
+
+    // gets updated all the time until loses target.
+    endTargetDistance = measuredDistance;
+    endTargetAngle = currentAngle;
+  }
+  else if (prevIsOnTarget) // Dropping edge
+  {
+    isShooting = true;
+    centerOfTarget = turret.getCenterOfTarget(startTargetAngle, startTargetDistance, endTargetAngle, endTargetDistance);
+  }
 };
