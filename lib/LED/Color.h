f01c1e19d02ba51f7db031a2c1d70ce3900d9705
NoamPrag <noam.prag07@gmail.com>
2020-12-07 21:30:24 +0200

    Formatted Color.h file

--- lib/LED/Color.h
+++ lib/LED/Color.h
@@ -1,11 +1,11 @@
 struct Color
 {
-    const unsigned int red;
-    const unsigned int green;
-    const unsigned int blue;
+  const unsigned int red;
+  const unsigned int green;
+  const unsigned int blue;
 
-    Color(const unsigned int red, const unsigned int green, const unsigned int blue)
-        : red(red),
-          green(green),
-          blue(blue){};
+  Color(const unsigned int red, const unsigned int green, const unsigned int blue)
+      : red(red),
+        green(green),
+        blue(blue){};
 };
